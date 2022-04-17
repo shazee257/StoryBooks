9f448c39d6816de6f5623bc8591eeb840e8caee8
Muhammad Shahzad <shazee257@gmail.com>
2022-04-17 12:52:59 +0500

    abc

--- app.js
+++ app.js
@@ -87,5 +87,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 );
